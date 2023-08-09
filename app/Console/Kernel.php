df52cfd6c74625077dbdac2bef357cbbfbdcac22
Flemming Petersen <mail@flemming-petersen.de>
2023-08-09 20:28:39 +0200

    chore: add cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('simple-borrow:send:reminder')->dailyAt('08:00');
         $schedule->command('simple-borrow:send:overdue')->dailyAt('08:00');
+        $schedule->log('info', 'Scheduled commands executed.');
     }
 
     /**
