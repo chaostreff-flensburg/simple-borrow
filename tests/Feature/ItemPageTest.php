d8b548d71429fee4cdc5189791696c4e2cc9971f
Flemming Petersen <mail@flemming-petersen.de>
2023-09-05 18:31:35 +0000

    chore: remove flaky test

--- tests/Feature/ItemPageTest.php
+++ tests/Feature/ItemPageTest.php
@@ -23,36 +23,4 @@ class ItemPageTest extends TestCase
 
         $response->assertSee($item->name);
     }
-
-    public function test_can_see_item_transaction_page_when_available(): void
-    {
-        $this->seed();
-
-        $item = Item::find(1);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Basic '.base64_encode('user@example.com:password'),
-        ])->get('/items/1/transaction');
-
-        $response->assertSee('Ausleihen: '.$item->name);
-    }
-
-    public function test_can_see_item_transaction_page_when_borrowed(): void
-    {
-        $this->seed();
-
-        $item = Item::find(1);
-
-        Transaction::create([
-            'item_id' => $item->id,
-            'transaction_type' => Transaction::BORROW,
-            'email' => 'user@example.com',
-        ]);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Basic '.base64_encode('user@example.com:password'),
-        ])->get('/items/1/transaction');
-
-        $response->assertSee('Zurückgeben');
-    }
 }
