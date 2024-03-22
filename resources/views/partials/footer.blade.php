4ed3cd469e05c0a6cfb1d86beb500cee54d80672
scammo <kontakt@samuelbrinkmann.de>
2024-03-22 22:47:52 +0000

    footer with links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,15 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-    <div>
-      <p>Chaostreff Flensburg</p>
-    </div>
-</footer>
+<footer class="footer p-10 bg-base-200 text-base-content">
+    <aside>
+      <p>Chaos Computer Club Flensburg e.V.</p>
+    </aside>
+    <nav>
+      <h6 class="footer-title">Rechtliches</h6>
+      <a class="link link-hover" href="https://chaostreff-flensburg.de/impressum/" target="_blank">Impressum</a>
+      <a class="link link-hover" href="https://chaostreff-flensburg.de/datenschutz/" target="_blank">Datenschutz</a>
+    </nav>
+    <nav>
+      <h6 class="footer-title">Technisches</h6>
+      <a class="link link-hover" href="https://github.com/chaostreff-flensburg/simple-borrow" target="_blank">Github</a>
+      <a class="link link-hover" href="https://github.com/chaostreff-flensburg/simple-borrow/issues/new" target="_blank">Fehler gefunden?</a>
+    </nav>
+  </footer>
