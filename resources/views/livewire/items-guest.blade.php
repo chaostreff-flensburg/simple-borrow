e8a2743cedbd9f33ee2458371b6bee63ee7031b5
Flemming Petersen <mail@flemming-petersen.de>
2024-03-23 14:53:12 +0100

    Center card imag

--- resources/views/livewire/items-guest.blade.php
+++ resources/views/livewire/items-guest.blade.php
@@ -1,8 +1,8 @@
 <div>
     <section class="pb-4 mb-4 border-b-2 border-solid">
-            <button class="badge badge-outline" wire:click="filter" >{{ __('All') }}</button>
+            <button class="badge badge-outline" wire:click="filter" >{{ __('All') }} {{ $items->count() }}</button>
             @foreach ($tags as $tag)
-                <button class="badge badge-outline" wire:click="filter({{ $tag->id }})" >{{ $tag->title }}</button>
+                <button class="badge badge-outline" wire:click="filter({{ $tag->id }})" >{{ $tag->title }} ({{ $tag->items->count() }})</button>
             @endforeach
     </section>
     <section class="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
